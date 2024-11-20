96ddb654559d01e58d83dd667d23c3ad7ec88e73
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-20 22:46:12 +0530

    Setup App Navigation

--- app/src/main/java/com/example/chatter/MainActivity.kt
+++ app/src/main/java/com/example/chatter/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.chatter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatter.ui.theme.ChatterTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +11,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ChatterTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppNavigation()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChatterTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/chatter/feature/auth/signin/SignInScreen.kt
@@ -0,0 +1,7 @@
+package com.example.chatter.feature.auth.signin
+
+import androidx.navigation.NavController
+
+fun SignInScreen(navController: NavController){
+    
+}
