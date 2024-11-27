e6ab85c8aea233920e24cf0e0b3a946cc70ee178
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-27 19:31:28 +0530

    Create Home View Model

--- app/src/main/java/com/example/chatter/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/example/chatter/feature/home/HomeViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.chatter.feature.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.chatter.model.Channel
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor() : ViewModel() {
+
+    val firebaseDatabase = Firebase.database
+    private val _channels = MutableStateFlow<List<Channel>>(emptyList())
+    val channels = _channels.asStateFlow()
+
+    init {
+        getChannels()
+    }
+
+    private fun getChannels() {
+        firebaseDatabase.getReference("channel").get().addOnSuccessListener {
+            val list = mutableListOf<Channel>()
+            it.children.forEach { data ->
+                val channel = Channel(data.key!!, data.key.toString())
+                list.add(channel)
+            }
+            _channels.value = list
+        }
+    }
 }
