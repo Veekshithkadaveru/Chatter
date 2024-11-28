a19465a3e786e0263527a58551b8b99620f2dc69
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-28 19:59:56 +0530

    Add Channel Dialog

--- app/src/main/java/com/example/chatter/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/chatter/feature/home/HomeScreen.kt
@@ -2,20 +2,29 @@ package com.example.chatter.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -26,7 +35,23 @@ fun HomeScreen(navController: NavController) {
     val viewModel = hiltViewModel<HomeViewModel>()
     val channels = viewModel.channels.collectAsState()
 
-    Scaffold { paddingValues ->
+    Scaffold(
+        floatingActionButton = {
+            Box(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clip(RoundedCornerShape(16.dp))
+                    .background(Color.Blue)
+                    .clickable { /* TODO*/ }
+            ) {
+                Text(
+                    text = "Add Channel",
+                    modifier = Modifier.padding(16.dp),
+                    color = Color.White
+                )
+            }
+        }
+    ) { paddingValues ->
         Box(
             modifier = Modifier
                 .padding(paddingValues)
@@ -36,7 +61,8 @@ fun HomeScreen(navController: NavController) {
                 items(channels.value) { channel ->
                     Text(
                         text = channel.name,
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier
+                            .fillMaxWidth()
                             .padding(8.dp)
                             .clip(RoundedCornerShape(16.dp))
                             .background(Color.Red.copy(alpha = 0.3f))
@@ -47,5 +73,41 @@ fun HomeScreen(navController: NavController) {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun AddChannelDialog(onAddChannel: (String) -> Unit) {
+    val channelName = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Add Channel")
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        TextField(value = channelName.value, onValueChange = {
+            channelName.value = it
+        }, label = { Text(text = "Channel Name") }, singleLine = true)
+
+        Spacer(modifier = Modifier.padding(8.dp))
+
+        Button(
+            onClick = { onAddChannel(channelName.value) },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Add")
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun AddChannelDialogPreview() {
+    AddChannelDialog {
+
     }
 }
