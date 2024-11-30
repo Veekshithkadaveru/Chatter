6585dca59f8235f785d83e5c630896c5236d1301
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-11-30 18:57:33 +0530

    Create Chat ViewModel

--- app/src/main/java/com/example/chatter/model/Message.kt
+++ app/src/main/java/com/example/chatter/model/Message.kt
@@ -3,7 +3,6 @@ package com.example.chatter.model
 data class Message(
     val id: String = "",
     val senderId: String = "",
-    val receiverId: String = "",
     val message: String = "",
     val createdAt: Long = System.currentTimeMillis(),
     val senderName: String = "",
