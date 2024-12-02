15d0b70c27c1ed1b900ff9df14178a0e5c1c3b96
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-12-02 19:46:49 +0530

    Add navigation route for chat

--- app/src/main/java/com/example/chatter/AppNavigation.kt
+++ app/src/main/java/com/example/chatter/AppNavigation.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.chatter.feature.auth.signin.SignInScreen
 import com.example.chatter.feature.auth.signup.SignUpScreen
+import com.example.chatter.feature.chat.ChatScreen
 import com.example.chatter.feature.home.HomeScreen
 import com.google.firebase.auth.FirebaseAuth
 
@@ -29,6 +32,14 @@ fun AppNavigation() {
             }
             composable("home") {
                 HomeScreen(navController)
+            }
+            composable("chat/{channel}", arguments = listOf(
+                navArgument("channelId") {
+                    type = NavType.StringType
+                }
+            )) {
+                val channelId = it.arguments?.getString("channelId") ?: ""
+                ChatScreen(navController = navController, channelId = channelId)
             }
         }
     }
